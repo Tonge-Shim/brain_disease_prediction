5ff07a6006183d65b16e9b5f2cbe24d84fd8271f
Tonge-Shim <tonge20@naver.com>
2021-11-03 17:54:18 +0900

    train.py

--- train.py
+++ train.py
@@ -13,7 +13,14 @@ import torch.nn as nn
 from sklearn.metrics import roc_auc_score, average_precision_score
 import tqdm
 from operator import add
+import wandb
 import matplotlib.pyplot as plt
 import easydict
 
-def train()
+def train(sliced_data, label_name, ):
+    data = np.load('./sliced_datas/' + sliced_data + '.npy')#ex.sliced_data = sliced_436
+    label = np.load('./labels/'+ label_name+ '.npy')
+    X_train = data[:621]
+    X_test = data[621:]
+    y_train = label[:621]
+    y_test = label[621:]
