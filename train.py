e2fa93e08802b7b8d927da6cdf3b5acaac04c7f8
Tonge-Shim <tonge@naver.com>
2021-11-03 18:03:25 +0900

    'ddd'

--- train.py
+++ train.py
@@ -24,3 +24,38 @@ def train(sliced_data, label_name, ):
     X_test = data[621:]
     y_train = label[:621]
     y_test = label[621:]
+    datadict = {
+        'X_train' : X_train,
+        'y_train' : y_train,
+        'X_test' : X_test,
+        'y_test' : y_test
+    }
+
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+    class mridataset(Dataset):
+    def __init__(self, data, data_type):
+        if data_type == 'train':
+            self.X = data['X_train']
+            self.y = data['y_train']
+        elif data_type == 'test':
+            self.X = data['X_test']
+            self.y = data['y_test']
+            
+        assert len(self.X) == len(self.y), "length should be same between input and label"
+        
+        self.X = torch.FloatTensor(self.X)#cpu tensor
+        self.y = torch.FloatTensor(self.y)#cpu tensor
+    
+    def __len__(self):
+        return len(self.y)
+    
+    def __getitem__(self, index):
+        X = self.X[index]
+        y = self.y[index]
+        return {
+            'X': X,
+            'y_target': y
+        }
+
+    
